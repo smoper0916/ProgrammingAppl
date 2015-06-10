2cf18a9101ddb862bad5adef5807e72185ea5a57
smoper0916 <smoper0916@gmail.com>
2015-06-10 14:07:37 +0900

    정상화
    ㅎ

--- header/IOHandler.h
+++ header/IOHandler.h
@@ -16,10 +16,7 @@ public:
 
 	virtual void save(void* object) = 0;
 	virtual void* load(string key="") = 0;
-	// key == ""면 파일 내용 전체를 불러옴. 리스트로 리턴할 때는 Vector로 Return
-	// void*는 모든 type을 받아오고 리턴할 수 있음. 쓸 때는 (객체*) 이렇게 캐스트 해주면 정상적으로 사용가능.
-	// 해당 객체가 어떤 객체인지 알고 싶으면 typeid(객체) == typeid(Class명) 을 이용하자. 해당 Class로 만든 객체이면 저게 true로 나옴.
-
+	virtual void initialization(void* object) = 0;
 private:
 	string fileName;
 };
--- header/Student.h
+++ header/Student.h
@@ -1,15 +1,21 @@
-#ifndef _STUDENT_H_
-#define _STUDENT_H_
+#pragma once
 
-#include <iostream>
+#include <string>
+
 using namespace std;
 
-class Student {
+class Student
+{
 public:
-
+	Student(string stNum, string name){ 
+		this->stNum = stNum;  
+		this->name = name;
+	}
+	void setStNum(string stNum){ this->stNum = stNum; }
+	void setName(string name){ this->name = name; }
+	string getStNum(){ return stNum; }
+	string getName(){ return name; }
 private:
-
-
+	string stNum;
+	string name;
 };
-
-#endif
