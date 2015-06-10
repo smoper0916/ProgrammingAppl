2ce2b775037ad7bc555cc29b606d7e8ec2c86923
swkim0128 <swkim0128@naver.com>
2015-06-10 20:26:45 +0900

    d
    d

--- header/EstablishSubject.h
+++ header/EstablishSubject.h
@@ -1,15 +1,28 @@
 #ifndef _ESTABLISHSUBJECT_H_
 #define _ESTABLISHSUBJECT_H_
 
+#include <vector>
 #include "Lecturer.h"
 #include "Subject.h"
+
+struct lectTime {
+	lectTime(string d, int t) : day(d), time(t) { }
+
+	string day;
+	int time;
+};
 
 class EstablishSubject {
 public:
 	EstablishSubject();
+	EstablishSubject(int y, int seme, string subNum, string lectNum, vector<lectTime> lcTime);
+
+	Subject getSubject();
 
 private:
+	Subject subject;
 	
+	vector<lectTime> lectureTime;
 };
 
 #endif
